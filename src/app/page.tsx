a9b195067865279c2e0ddbff8c02cafb189d6fad
achinthajayaweera <achinthajayaweera26@gmail.com>
2026-06-10 03:44:43 +0530

    v1: project scaffolding - configs, theme, types, constants, Navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <div>
+        {/* Sections will be added in upcoming versions */}
+      </div>
+    </main>
+  );
+}
