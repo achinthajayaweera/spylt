37404562e206c000babafe294aa8695be2d04a40
achinthajayaweera <achinthajayaweera26@gmail.com>
2026-06-11 03:12:27 +0530

    v12: final polish - ScrollSmoother, global GSAP registration, refined styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,67 @@
 "use client";
-import { useState } from "react";
 import Navbar from "@/components/Navbar";
-import Preloader from "@/components/Preloader";
 import HeroSection from "@/sections/HeroSection";
+import gsap from "gsap";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 import MessageSection from "@/sections/MessageSection";
-import FlavorSection from "@/sections/FlavorSection";
 import NutritionSection from "@/sections/NutritionSection";
+import FlavorSection from "@/sections/FlavorSection";
 import BenefitSection from "@/sections/BenefitSection";
 import TestimonialSection from "@/sections/TestimonialSection";
 import FooterSection from "@/sections/FooterSection";
+import { useState, useEffect } from "react";
+import Preloader from "@/components/Preloader";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Home() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [triggerAnimation, setTriggerAnimation] = useState(false);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const [isPreloaderDone, setIsPreloaderDone] = useState(false);
+
+  useEffect(() => {
+    window.history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+  }, []);
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
 
   return (
     <main>
       <Preloader
-        isLoaded={isLoaded}
-        onFinish={() => setTriggerAnimation(true)}
+        isLoaded={isVideoLoaded}
+        onFinish={() => setIsPreloaderDone(true)}
       />
-      <Navbar />
-      <HeroSection
-        onLoaded={() => setIsLoaded(true)}
-        triggerAnimation={triggerAnimation}
-      />
-      <MessageSection />
-      <FlavorSection />
-      <NutritionSection />
-      <BenefitSection />
-      <TestimonialSection />
-      <FooterSection />
+      <div
+        className={
+          !isPreloaderDone
+            ? "h-screen overflow-hidden opacity-0"
+            : "opacity-100 transition-opacity duration-500"
+        }
+      >
+        <Navbar />
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <HeroSection
+              onLoaded={() => setIsVideoLoaded(true)}
+              triggerAnimation={isPreloaderDone}
+            />
+            <MessageSection />
+            <FlavorSection />
+            <NutritionSection />
+            <div>
+              <BenefitSection />
+              <TestimonialSection />
+            </div>
+            <FooterSection />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
